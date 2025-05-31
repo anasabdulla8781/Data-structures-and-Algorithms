c799650873c4f276ba20a6e83fbe7b681486cacb
Anas abdulla <anasabdulla8781@gmail.com>
2025-05-31 22:51:44 +0530

    first file in binary tree and the folder structure

---
+++ Binary Tree/00 - Binary tree starting.c
@@ -0,0 +1,1 @@
+/// this folder contains all the works in the binary tree 
---
+++ Linked list/13 - Deleting first node from linked list.c
@@ -0,0 +1,210 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node 
+{
+    int data;
+    struct node *next ;
+};
+
+struct node *head = NULL;
+struct node *tail = NULL;
+
+void insert_node(int data)
+{
+    struct node * temp = (struct node *)malloc(sizeof(struct node));
+    temp->data = data;
+    temp->next = NULL;
+
+    if (!head)
+    {
+        head = temp;
+        tail = head;
+    }
+    else
+    {
+        tail->next= temp;
+        tail = tail->next;
+    }
+}
+
+void display_node(void)
+{
+    if(!head)
+    {
+        printf("there is no lists");
+    }
+    else
+    {
+        struct node *temp = head;
+        while(temp)
+        {
+            printf(" %d",temp->data);
+            temp = temp->next;
+        }
+    }
+}
+
+void sum_of_nodes(void)
+{
+    struct node* temp = head;
+    int sum = 0;
+
+    while (temp)
+    {
+        sum = sum + temp->data;
+        temp = temp->next;
+    }
+    printf ("\nthe sum of nodes are %d",sum);
+}
+
+void max_element_in_list(void)
+{
+    struct node *temp = head;
+    int max = -32768;               /// DONT GIVE 0 Here . give the minimum number of integer element possible
+
+    while (temp)
+    {
+        if (max < (temp->data))
+        {
+            max = temp->data;
+        }
+        temp = temp->next;
+    }
+    if(head)
+    {
+        printf("\nThe max element is %d",max);
+    }
+    else
+    {
+        printf("\nThere is no elements in the list");
+    }
+    
+}
+
+void search_node_linear(int data)
+{
+    struct node *temp = head;
+    int count = 1;
+    if(!head)
+    {
+        printf("\nThere is no list itself\n");
+    }
+    else
+    {
+        while (temp)
+        {
+            if(data == temp->data)
+            {
+                printf("\nThe element is available in %dth position in list\n",count);
+                break;
+            }
+            else
+            {
+                temp = temp->next;
+                count++;
+            }
+        }
+        if(!temp)
+        {
+            printf ("\nThere is no node with the element we searched\n");
+        }
+        
+    }
+}
+
+void search_and_move(int data)
+{
+    struct node* temp = head;
+    struct node* temp2 = head;
+
+    while (temp)
+    {
+        if(temp->data == data)
+        {
+            if (temp == head)
+            {
+                break;
+            }
+            temp2->next = temp->next;
+            temp->next = head;
+            head = temp;
+            break;
+        }
+        else
+        {
+            temp2 = temp;
+            temp = temp->next;
+        }
+    }
+}
+
+void insert_node_location (int data , int location)
+{
+    struct node *new = (struct node*)malloc(sizeof(struct node));
+    new->data = data;
+    new->next = NULL;
+
+    struct node*temp = head;
+    int i = 1;
+
+    if (location == 1)
+    {
+        new->next = temp;
+        head = new;
+    }
+    else
+    {
+        while (temp && ((i+1)<location))
+        {
+            temp = temp->next;
+            i++;
+        }
+        
+        if(temp)
+        {
+            new->next = temp->next;
+            temp->next = new;
+        }
+        else
+        {
+            insert_node(data);
+        }
+
+    }
+
+}
+
+void delete_first_node(void)
+{
+    struct node *temp= head;
+
+    if(head)
+    {
+        head = head->next;
+        temp->next = NULL;
+        free(temp);
+    }
+
+}
+
+int main ()
+{
+
+    insert_node(10);
+    insert_node(26);
+    insert_node(4);
+    insert_node(36);
+    insert_node(7);
+    display_node();
+    // sum_of_nodes();
+    // max_element_in_list();
+    // search_node_linear(6);
+    // search_and_move(36);
+    // insert_node_location(63,58);
+    // display_node();
+    delete_first_node();
+    display_node();
+
+    return 0;
+}
